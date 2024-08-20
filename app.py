bce10b3318277969d5730575d8667c961153ab3c
Meenal_Sharma <meenalsharma2003@gmail.com>
2024-08-20 13:30:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from datetime import datetime
 import numpy as np
 import pandas as pd
-import seaborn as sns
+# import seaborn as sns
 import matplotlib.pyplot as plt
 
 import plotly.express as px
